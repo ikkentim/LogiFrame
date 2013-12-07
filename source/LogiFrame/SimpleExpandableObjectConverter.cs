4edaa5377cc335606e5d09211d650b3a4cc85364
Tim Potze <timpotze@gmail.com>
2013-12-07 22:02:17 +0100

    Fixed NullPointerException

--- source/LogiFrame/SimpleExpandableObjectConverter.cs
+++ source/LogiFrame/SimpleExpandableObjectConverter.cs
@@ -27,7 +27,7 @@ namespace LogiFrame
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value,
             Type destinationType)
         {
-            return destinationType == typeof (string)
+            return destinationType == typeof (string) && value != null
                 ? value.ToString()
                 : base.ConvertTo(context, culture, value, destinationType);
         }
