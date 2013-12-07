42c11b8e90a6bd612b9d754758f503374c7877e2
Tim Potze <timpotze@gmail.com>
2013-12-07 16:03:06 +0100

    Made Simulation form resizable

--- source/LogiFrame/Simulation.Designer.cs
+++ source/LogiFrame/Simulation.Designer.cs
@@ -36,11 +36,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.propertyGrid = new System.Windows.Forms.PropertyGrid();
-            this.propertiesGroupBox = new System.Windows.Forms.GroupBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel = new System.Windows.Forms.Panel();
+            this.propertiesGroupBox = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.displayPictureBox)).BeginInit();
             this.displayGroupBox.SuspendLayout();
             this.buttonsGroupBox.SuspendLayout();
+            this.panel.SuspendLayout();
             this.propertiesGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +57,7 @@
             // displayGroupBox
             // 
             this.displayGroupBox.Controls.Add(this.displayPictureBox);
-            this.displayGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.displayGroupBox.Location = new System.Drawing.Point(3, 3);
             this.displayGroupBox.Name = "displayGroupBox";
             this.displayGroupBox.Size = new System.Drawing.Size(173, 72);
             this.displayGroupBox.TabIndex = 1;
@@ -68,7 +70,7 @@
             this.buttonsGroupBox.Controls.Add(this.button3);
             this.buttonsGroupBox.Controls.Add(this.button2);
             this.buttonsGroupBox.Controls.Add(this.button1);
-            this.buttonsGroupBox.Location = new System.Drawing.Point(12, 90);
+            this.buttonsGroupBox.Location = new System.Drawing.Point(3, 81);
             this.buttonsGroupBox.Name = "buttonsGroupBox";
             this.buttonsGroupBox.Size = new System.Drawing.Size(173, 64);
             this.buttonsGroupBox.TabIndex = 2;
@@ -122,24 +124,15 @@
             this.propertyGrid.Location = new System.Drawing.Point(3, 16);
             this.propertyGrid.Name = "propertyGrid";
             this.propertyGrid.PropertySort = System.Windows.Forms.PropertySort.Alphabetical;
-            this.propertyGrid.Size = new System.Drawing.Size(263, 222);
+            this.propertyGrid.Size = new System.Drawing.Size(225, 172);
             this.propertyGrid.TabIndex = 3;
             this.propertyGrid.ToolbarVisible = false;
             // 
-            // propertiesGroupBox
-            // 
-            this.propertiesGroupBox.Controls.Add(this.propertyGrid);
-            this.propertiesGroupBox.Location = new System.Drawing.Point(191, 12);
-            this.propertiesGroupBox.Name = "propertiesGroupBox";
-            this.propertiesGroupBox.Size = new System.Drawing.Size(269, 241);
-            this.propertiesGroupBox.TabIndex = 4;
-            this.propertiesGroupBox.TabStop = false;
-            this.propertiesGroupBox.Text = "Properties";
-            // 
             // linkLabel1
             // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(15, 237);
+            this.linkLabel1.Location = new System.Drawing.Point(6, 163);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(126, 13);
             this.linkLabel1.TabIndex = 5;
@@ -147,19 +140,42 @@
             this.linkLabel1.Text = "LogiFrame, by @ikkentim";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.displayGroupBox);
+            this.panel.Controls.Add(this.linkLabel1);
+            this.panel.Controls.Add(this.buttonsGroupBox);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel.Location = new System.Drawing.Point(8, 8);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(178, 191);
+            this.panel.TabIndex = 6;
+            // 
+            // propertiesGroupBox
+            // 
+            this.propertiesGroupBox.Controls.Add(this.propertyGrid);
+            this.propertiesGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertiesGroupBox.Location = new System.Drawing.Point(186, 8);
+            this.propertiesGroupBox.Margin = new System.Windows.Forms.Padding(10);
+            this.propertiesGroupBox.Name = "propertiesGroupBox";
+            this.propertiesGroupBox.Size = new System.Drawing.Size(231, 191);
+            this.propertiesGroupBox.TabIndex = 7;
+            this.propertiesGroupBox.TabStop = false;
+            this.propertiesGroupBox.Text = "Properties";
+            // 
             // Simulation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(472, 265);
-            this.Controls.Add(this.linkLabel1);
+            this.ClientSize = new System.Drawing.Size(425, 207);
             this.Controls.Add(this.propertiesGroupBox);
-            this.Controls.Add(this.buttonsGroupBox);
-            this.Controls.Add(this.displayGroupBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(210, 237);
             this.Name = "Simulation";
+            this.Padding = new System.Windows.Forms.Padding(8);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Simulation";
@@ -167,9 +183,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.displayPictureBox)).EndInit();
             this.displayGroupBox.ResumeLayout(false);
             this.buttonsGroupBox.ResumeLayout(false);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
             this.propertiesGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -183,7 +200,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PropertyGrid propertyGrid;
-        private System.Windows.Forms.GroupBox propertiesGroupBox;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.GroupBox propertiesGroupBox;
     }
 }
