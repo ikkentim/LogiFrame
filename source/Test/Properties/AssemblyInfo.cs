9c39524504f2d5b70a97d3d3a16e445d295919d1
Tim Potze <timpotze@gmail.com>
2013-12-07 16:07:53 +0100

    Changed Test properties

--- source/Test/Properties/AssemblyInfo.cs
+++ source/Test/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LogiFrame Test application")]
+[assembly: AssemblyDescription("LogiFrame Test application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("LogiFrame Test application")]
+[assembly: AssemblyCopyright("Copyright © ikkentim 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
